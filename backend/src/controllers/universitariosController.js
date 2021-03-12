d9ece312288de9943b1ca035f38333f36f6ece5f
Adolfo <adlf.aldana@gmail.com>
2021-03-12 06:51:13 -0400

    obteniendo a universitarios y guardando a universitario

--- backend/src/controllers/universitariosController.js
+++ backend/src/controllers/universitariosController.js
@@ -1,17 +1,35 @@
 const univerCtrl = {};
+const universitarioModel = require("../models/universitarioModels");
 
-univerCtrl.getUniversitarios = (req, res) =>
-  res.send("Datos de Universitarios");
+// OBTIENE A TODOS LOS UNIVERSITARIOS
+univerCtrl.getUniversitarios = async (req, res) => {
+  // consultando
+  const universitario = await universitarioModel.find();
+  // console.log(universitario);
+  res.json(universitario);
+};
 
-univerCtrl.createUniversitario = (req, res) =>
-  res.send("Creando universitario");
+// GUARDA UN NUEVO UNIVERSTARIO
+univerCtrl.createUniversitario = async (req, res) => {
+  const { nombre, apellidos } = req.body;
+  const newUniversitario = new universitarioModel({
+    nombre,
+    apellidos,
+  });
+  // console.log(newUniversitario);
+  await newUniversitario.save();
+  res.send({ msg: "universitario Guardado" });
+};
 
+// OBTIENE A UN UNIVERSITARIO POR ID
 univerCtrl.getUniversitario = (req, res) =>
   res.send("Datos de un universitario");
 
+// ACTUALIZA UN UNIVERSITARIO POR ID
 univerCtrl.updateUniversitario = (req, res) =>
   res.send("Actualizando Universitario");
 
+// ELIMINA A UN UNIVERSITARIO POR ID
 univerCtrl.deleteUniversitario = (req, res) =>
   res.send("Eliminando Universitario");
 
