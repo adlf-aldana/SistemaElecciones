5bfe2a8292394f8c6fe8df54c2d84e99f8d81232
Adolfo <adlf.aldana@gmail.com>
2021-03-12 10:39:33 -0400

    editar y eliminar universitario

--- backend/src/controllers/universitariosController.js
+++ backend/src/controllers/universitariosController.js
@@ -22,15 +22,25 @@ univerCtrl.createUniversitario = async (req, res) => {
 };
 
 // OBTIENE A UN UNIVERSITARIO POR ID
-univerCtrl.getUniversitario = (req, res) =>
-  res.send("Datos de un universitario");
+univerCtrl.getUniversitario = async (req, res) => {
+  const universitario = await universitarioModel.findById(req.params.id);
+  res.json({ msg: universitario });
+};
 
 // ACTUALIZA UN UNIVERSITARIO POR ID
-univerCtrl.updateUniversitario = (req, res) =>
-  res.send("Actualizando Universitario");
+univerCtrl.updateUniversitario = async (req, res) => {
+  const { nombre, apellidos } = req.body;
+  await universitarioModel.findOneAndUpdate(req.params.id, {
+    nombre,
+    apellidos,
+  });
+  res.json({ msg: "Universitario actualizado" });
+};
 
 // ELIMINA A UN UNIVERSITARIO POR ID
-univerCtrl.deleteUniversitario = (req, res) =>
-  res.send("Eliminando Universitario");
+univerCtrl.deleteUniversitario = async (req, res) => {
+  await universitarioModel.findOneAndDelete(req.params.id);
+  res.json({ msg: "Universitario eliminado" });
+};
 
 module.exports = univerCtrl;
--- backend/src/database.js
+++ backend/src/database.js
@@ -9,6 +9,8 @@ const URI = process.env.MONGODB_URI
 mongoose.connect(URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 const connection = mongoose.connection;
