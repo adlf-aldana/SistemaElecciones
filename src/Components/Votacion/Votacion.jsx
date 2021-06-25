850911ab7aff163a3883cb64c2887682eb10505d
Adolfo <adlf.aldana@gmail.com>
2021-06-24 23:13:28 -0400

    mostrando mensaje al realizar una votacion

--- src/Components/Votacion/Votacion.jsx
+++ src/Components/Votacion/Votacion.jsx
@@ -38,65 +38,53 @@ const Votacion = () => {
   }, []);
   return (
     <Fragment>
-      {
-        autorizandoVotante ? (
-          autorizandoVotante.estadoEncargadoMesa &&
-          autorizandoVotante.estadoVerificadorVotante &&
-          autorizandoVotante._idFrente === null ? (
-            <div className="container mt-4">
-              <Cards estudiante={autorizandoVotante} usuario={null} />
-              <h1 className="text-center mt-4">Votación</h1>
-              <div className="row">
-                {frentes.length > 0 ? (
-                  frentes.map((frente) => (
-                    <div className="col-md-4 mt-3" key={frente._id}>
-                      <div
-                        className="card text-center"
-                        style={{ width: "18rem", display: "block" }}
-                      >
-                        <img
-                          src={`http://localhost:4000/${frente.logoFrente}`}
-                          // src={`http://192.168.0.6:4000/${frente.logoFrente}`}
-                          alt="..."
-                          width="150"
-                          height="160"
-                          className="mt-3"
-                        />
-                        <div className="card-body">
-                          <h5 className="card-title">
-                            {crypto.AES.decrypt(
-                              frente.nombreFrente,
-                              "palabraClave"
-                            ).toString(crypto.enc.Utf8)}
-                          </h5>
-                          <button
-                            className="btn btn-primary"
-                            onClick={() => btnVotar(frente)}
-                          >
-                            Votar
-                          </button>
-                        </div>
+      {confirmado ? (
+        <h3 className="text-center mt-5">Voto Realizado Correctamente</h3>
+      ) : autorizandoVotante ? (
+        autorizandoVotante.estadoEncargadoMesa &&
+        autorizandoVotante.estadoVerificadorVotante &&
+        autorizandoVotante._idFrente === null ? (
+          <div className="container mt-4">
+            <Cards estudiante={autorizandoVotante} usuario={null} />
+            <h1 className="text-center mt-4">Votación</h1>
+            <div className="row">
+              {frentes.length > 0 ? (
+                frentes.map((frente) => (
+                  <div className="col-md-4 mt-3" key={frente._id}>
+                    <div
+                      className="card text-center"
+                      style={{ width: "18rem", display: "block" }}
+                    >
+                      <img
+                        src={`http://localhost:4000/${frente.logoFrente}`}
+                        // src={`http://192.168.0.6:4000/${frente.logoFrente}`}
+                        alt="..."
+                        width="150"
+                        height="160"
+                        className="mt-3"
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          {crypto.AES.decrypt(
+                            frente.nombreFrente,
+                            "palabraClave"
+                          ).toString(crypto.enc.Utf8)}
+                        </h5>
+                        <button
+                          className="btn btn-primary"
+                          onClick={() => btnVotar(frente)}
+                        >
+                          Votar
+                        </button>
                       </div>
                     </div>
-                  ))
-                ) : (
-                  <p>Sin datos</p>
-                )}
-              </div>
+                  </div>
+                ))
+              ) : (
+                <p>Sin datos</p>
+              )}
             </div>
-          ) : (
-            <div className="text-center mt-3">
-              <h3>ADVERTENCIA</h3>
-              <hr />
-              <h4>Antes realizar su voto verifique sus datos por favor</h4>
-              <button
-                className="btn btn-success mt-4"
-                onClick={() => ultimoVotante()}
-              >
-                Empezar
-              </button>
-            </div>
-          )
+          </div>
         ) : (
           <div className="text-center mt-3">
             <h3>ADVERTENCIA</h3>
@@ -110,9 +98,19 @@ const Votacion = () => {
             </button>
           </div>
         )
-}
-
-      {confirmado ? <p>Voto Realizado Correctamente</p> : null}
+      ) : (
+        <div className="text-center mt-3">
+          <h3>ADVERTENCIA</h3>
+          <hr />
+          <h4>Antes realizar su voto verifique sus datos por favor</h4>
+          <button
+            className="btn btn-success mt-4"
+            onClick={() => ultimoVotante()}
+          >
+            Empezar
+          </button>
+        </div>
+      )}
     </Fragment>
   );
 };
