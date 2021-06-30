14bbceb30daa669a4736bce0c8388c2fe661f16e
Adolfo <adlf.aldana@gmail.com>
2021-06-29 22:53:50 -0400

    ajustes para deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,18 @@
 require("dotenv").config();
 
 const app = require("./app");
+const port = process.env.port || 4000;
 // Llamando a database, creación y coneccion a la base de datos mongoDB
 require("./database");
 
 // será la encargada de iniciar el programa
 async function main() {
   //Inicializando servidor
-  await app.listen(app.get("port"), "0.0.0.0");
-  console.log("Server on port ", app.get("port"));
+  app.listen(port, "0.0.0.0", () => {
+  console.log(`Server on port ${port}`);
+  })
+  // await app.listen(app.get("port"), "0.0.0.0");
+  // console.log("Server on port ", app.get("port"));
 }
 
 main();
